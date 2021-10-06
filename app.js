add31222ab4687e7916ca720e1b137fb23cd428d
Jackie <taekwondo.chan@gmail.com>
2021-10-05 19:49:48 -0700

    channel creation

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const server = require("http").Server(app);
 
 //Socket.io
 const io = require("socket.io")(server);
+let onlineUsers = {};
+//Save the channels in this object.
+let channels = { General: [] };
+
 io.on("connection", (socket) => {
-	require("./sockets/chat")(io, socket, onlineUsers);
+	// Make sure to send the channels to our chat file
+	require("./sockets/chat.js")(io, socket, onlineUsers, channels);
 });
 
 //Express View Engine for Handlebars
